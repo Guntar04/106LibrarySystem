c6296953afeee7330d642afdcef72d0183210597
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-11-08 13:12:24 +1300

    IDK

--- 106.2 LibrarySystem/106.2 LibrarySystem/MainWindow.xaml.cs
+++ 106.2 LibrarySystem/106.2 LibrarySystem/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace _106._2_LibrarySystem
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
