7e0ba117dcadbc77022bfb5475e85da1793daabb
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-11-23 09:13:01 +1300

    Working on BookDetails

--- 106LibrarySystem/MemberBookDetail.xaml.cs
+++ 106LibrarySystem/MemberBookDetail.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryDatabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,4 +26,5 @@ namespace _106LibrarySystem
             InitializeComponent();
         }
     }
+    
 }
