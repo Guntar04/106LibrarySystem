d11861e27269c76d939f8984d75462dcb31f4067
Guntar <arthur.h.werner@gmail.com>
2023-11-25 15:48:48 +1300

    Changed LoginPage to the main login
    I made MainWindow auto direct the user to login page

--- 106LibrarySystem/LoginPage.xaml.cs
+++ 106LibrarySystem/LoginPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +25,50 @@ namespace _106LibrarySystem
     {
         HomeScreen homePage = new HomeScreen();
         SignUp signUp = new SignUp();
+
         public LoginPage()
         {
             InitializeComponent();
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            LoginContent.Content = homePage;
-        }
+            string username = ItemBox.Text;
+            string password = ItemBox2.Text;
+            string databaseFileName = "LibraryDatabase.db";
+            string source = $"Data Source={System.IO.Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
 
+            // Establish a connection with the SQLite database
+            using (IDbConnection connection = new SQLiteConnection(source))
+            {
+                connection.Open();
+
+                // Query the database for a user with the entered username and password
+                string query = "SELECT COUNT(*) FROM users WHERE userName = @userName AND password = @password";
+                using (SQLiteCommand command = new SQLiteCommand(query, (SQLiteConnection)connection))
+                {
+                    command.Parameters.AddWithValue("@userName", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        // User authenticated, grant access to the application
+                        MessageBox.Show("Login Successful!");
+                        MainContent.Content = homePage;
+                    }
+                    else
+                    {
+                        // Show an error message indicating invalid credentials
+                        MessageBox.Show("Invalid username or password");
+                    }
+                }
+            }
+        }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            LoginContent.Content = signUp;
+            MainContent.Content = signUp;
         }
     }
 }
--- 106LibrarySystem/MainWindow.xaml.cs
+++ 106LibrarySystem/MainWindow.xaml.cs
@@ -23,52 +23,14 @@ namespace _106LibrarySystem
     /// </summary>
     public partial class MainWindow : Window
     {
-        HomeScreen homePage = new HomeScreen();
-        SignUp signUp = new SignUp();
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            string username = ItemBox.Text;
-            string password = ItemBox2.Text;
-            string databaseFileName = "LibraryDatabase.db";
-            string source = $"Data Source={System.IO.Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
-
-            // Establish a connection with the SQLite database
-            using (IDbConnection connection = new SQLiteConnection(source))
-            {
-                connection.Open();
-
-                // Query the database for a user with the entered username and password
-                string query = "SELECT COUNT(*) FROM users WHERE userName = @userName AND password = @password";
-                using (SQLiteCommand command = new SQLiteCommand(query, (SQLiteConnection)connection))
-                {
-                    command.Parameters.AddWithValue("@userName", username);
-                    command.Parameters.AddWithValue("@password", password);
-
-                    int count = Convert.ToInt32(command.ExecuteScalar());
-
-                    if (count > 0)
-                    {
-                        // User authenticated, grant access to the application
-                        MessageBox.Show("Login Successful!");
-                        MainContent.Content = homePage;
-                    }
-                    else
-                    {
-                        // Show an error message indicating invalid credentials
-                        MessageBox.Show("Invalid username or password");
-                    }
-                }
-            }
-        }
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = signUp;
+            LoginPage loginPage = new LoginPage();
+            loginCover.Content = loginPage;
         }
     }
 }
