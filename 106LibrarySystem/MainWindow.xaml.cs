dbaaf929ca36f7130f8741fe8da9e6ca5a98342f
Guntar <arthur.h.werner@gmail.com>
2023-11-25 13:57:48 +1300

    Login Working
    Managed to get the login to work but still need to navigate the members and admins to differnet pages

--- 106LibrarySystem/MainWindow.xaml.cs
+++ 106LibrarySystem/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,10 +33,39 @@ namespace _106LibrarySystem
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Login Successful!");
-            MainContent.Content = homePage;
-        }
+            string username = ItemBox.Text;
+            string password = ItemBox2.Text;
+            string databaseFileName = "LibraryDatabase.db";
+            string source = $"Data Source={System.IO.Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
 
+            // Establish a connection with the SQLite database
+            using (IDbConnection connection = new SQLiteConnection(source))
+            {
+                connection.Open();
+
+                // Query the database for a user with the entered username and password
+                string query = "SELECT COUNT(*) FROM users WHERE userName = @userName AND password = @password";
+                using (SQLiteCommand command = new SQLiteCommand(query, (SQLiteConnection)connection))
+                {
+                    command.Parameters.AddWithValue("@userName", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        // User authenticated, grant access to the application
+                        MessageBox.Show("Login Successful!");
+                        MainContent.Content = homePage;
+                    }
+                    else
+                    {
+                        // Show an error message indicating invalid credentials
+                        MessageBox.Show("Invalid username or password");
+                    }
+                }
+            }
+        }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MainContent.Content = signUp;
