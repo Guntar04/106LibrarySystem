5caf4f326ccc35cce05180d1adfc7f557b6a9ac7
oliviafatu <135172440+oliviafatu@users.noreply.github.com>
2023-11-28 09:26:24 +1300

    browsing page
    fixed add book, added remove book and some other features to the add book window like uploading picture and input fields

--- 106LibrarySystem/BookViewModel.cs
+++ 106LibrarySystem/BookViewModel.cs
@@ -1,27 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data.SQLite;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.IO;
-using System.IO.Packaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
 using Dapper;
 
 namespace LibraryDatabase
 {
-    public class BookViewModel
+    public class BookViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>();
 
-        public BookViewModel() {
 
-            LoadBooks();
+        private ObservableCollection<Book> _books = new ObservableCollection<Book>();
+
+        public ObservableCollection<Book> Books
+        {
+            get { return _books; }
+            set
+            {
+                if (_books != value)
+                {
+                    _books = value;
+                    OnPropertyChanged(nameof(Books));
+                }
+            }
         }
 
-        private void LoadBooks()
+        public BookViewModel()
+        {
+            LoadBooks();
+            CurrentBook = new Book();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void LoadBooks()
         {
             string databaseFileName = "LibraryDatabase.db";
             string source = $"Data Source={Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
@@ -34,17 +57,108 @@ namespace LibraryDatabase
                 foreach (var book in books)
                 {
                     book.ImagePath = GetImagePathFromDatabase(connection, book.Id);
+
+                    if (book.Id == 0)
+                    {
+                        // If the Id is not set, retrieve the correct Id from the database
+                        book.Id = connection.ExecuteScalar<int>("SELECT last_insert_rowid()");
+                    }
+
                     Books.Add(book);
                 }
             }
         }
 
         private string GetImagePathFromDatabase(IDbConnection connection, int bookId)
-        { 
-
+        {
             // Example query (replace with your actual database schema and query)
             string query = "SELECT ImagePath FROM books WHERE Id = @BookId";
-            return connection.ExecuteScalar<string>(query, new { BookId = bookId }) ?? ""; // Replace with the actual implementation
+            return connection.ExecuteScalar<string>(query, new { BookId = bookId }) ?? "";
         }
+
+        public void AddBook(Book newBook)
+        {
+            // Save the new book to the database
+            string databaseFileName = "LibraryDatabase.db";
+            string source = $"Data Source={Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
+
+            using (IDbConnection connection = new SQLiteConnection(source))
+            {
+                connection.Open();
+
+                connection.Execute(
+                    "INSERT INTO books (name, author, genre, availability, language, pageNum, date, ImagePath) " +
+                    "VALUES (@name, @author, @genre, @availability, @language, @pageNum, @date, @ImagePath)",
+                    new
+                    {
+                        name = newBook.name,
+                        author = newBook.author,
+                        genre = newBook.genre,
+                        availability = newBook.availability,
+                        language = newBook.language,
+                        pageNum = newBook.pageNum,
+                        date = newBook.date,
+                        ImagePath = newBook.ImagePath
+                    });
+
+                int lastInsertedId = connection.ExecuteScalar<int>("SELECT last_insert_rowid()");
+
+                newBook.Id = lastInsertedId;
+                newBook.ImagePath = GetImagePathFromDatabase(connection, lastInsertedId);
+
+                Books.Add(newBook);
+
+            }
+        }
+
+
+        private Book _currentBook;
+
+        public Book CurrentBook
+        {
+            get { return _currentBook; }
+            set
+            {
+                if (_currentBook != value)
+                {
+                    _currentBook = value;
+                    OnPropertyChanged(nameof(CurrentBook));
+                }
+            }
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (book == null)
+            {
+                // Handle the case where 'book' is null (optional)
+                return;
+            }
+
+            // Remove the book from the database
+            string databaseFileName = "LibraryDatabase.db";
+            string source = $"Data Source={Path.Combine(Directory.GetCurrentDirectory(), databaseFileName)}";
+
+            using (IDbConnection connection = new SQLiteConnection(source))
+            {
+                connection.Open();
+
+                // Add a null check for 'book.Id'
+                if (book.Id != 0)
+                {
+                    connection.Execute("DELETE FROM books WHERE Id = @BookId", new { BookId = book.Id });
+                }
+                else
+                {
+                    // Log or handle the case where 'book.Id' is 0
+                }
+            }
+
+            // Remove the book from the collection
+            Books.Remove(book);
+        }
+
+
+
     }
 }
