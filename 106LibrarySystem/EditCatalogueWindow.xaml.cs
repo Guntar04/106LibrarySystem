15e2d17eddf8f74a07956481399068fa198adde7
oliviafatu <135172440+oliviafatu@users.noreply.github.com>
2023-11-28 21:55:53 +1300

    start of the edit window

---
+++ 106LibrarySystem/EditCatalogueWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LibraryDatabase;
+
+namespace _106LibrarySystem
+{
+    /// <summary>
+    /// Interaction logic for EditCatalogueWindow.xaml
+    /// </summary>
+    public partial class EditCatalogueWindow : Window
+    {
+
+        public EditCatalogueWindow()
+        {
+            InitializeComponent();
+            var bookViewModel = new BookViewModel();
+            DataContext = bookViewModel;
+        }
+
+       
+    }
+}
